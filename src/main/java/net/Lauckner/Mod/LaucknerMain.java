15d1826dad8c8109bcd1bb1e32ce0f302041db54
BuildTools <unconfigured@null.spigotmc.org>
2016-02-26 20:33:17 +0100

    Lots of changes
    XD

---
+++ src/main/java/net/Lauckner/Mod/LaucknerMain.java
@@ -0,0 +1,37 @@
+package net.Lauckner.Mod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = LaucknerMain.MODID, version = LaucknerMain.VERSION)
+public class LaucknerMain
+{
+    public static final String MODID = "LaucknerMod";
+    public static final String VERSION = "a1.0";
+    
+    
+    
+    @EventHandler
+    public void preinit(FMLPreInitializationEvent event)
+    {
+		
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		
+    }
+    
+    
+    @EventHandler
+    public void postinit(FMLPostInitializationEvent event)
+    {
+		
+    	
+    }
+}
